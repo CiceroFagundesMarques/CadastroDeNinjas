b0de696368c0d052b0f15d51ec22460bcfaa6682
Cícero Fagundes Marqaues <cicero.marques@cedup.com>
2026-04-02 19:32:35 -0300

    Primeira controller

---
+++ src/main/java/com/circinho/CadastroDeNinjas/controller.java
@@ -0,0 +1,18 @@
+package com.circinho.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class controller {
+	
+	@GetMapping("/boasvindas")
+	public String boasVindas() {
+		
+		return "Minha primeira controller uhuhuhuhu";
+	}
+	
+
+}
